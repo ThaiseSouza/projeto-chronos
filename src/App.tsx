d7c1ffd289a42e5ea73eaae31bf721b66284f7be
THAISE VILLA SOUZA <05259183029@senacrs.edu.br>
2026-05-15 12:15:28 -0300

    Olá Thaise

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import "./styles/theme.css"
 export function App(){
 return(
 <Fragment>
-<h1>Olá Mundo!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dolorum voluptatem eum vero non error autem blanditiis incidunt nemo minus voluptatum nesciunt quas earum ea culpa, ad fuga obcaecati aliquid.</p>
 </Fragment>
 )
